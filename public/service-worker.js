5e68706892cfc3709ee36af14ee02a22916ed765
Broc Seib <broc.seib@gmail.com>
2019-11-15 18:45:24 -0500

    precache the dog.svg in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,11 @@
 
 self.addEventListener('install', event => {
   console.log(': service worker is installing');
+  event.waitUntil(
+    caches.open('my-svg-cache').then( cache => {
+      cache.add('/dog.svg');
+    })
+  );
 });
 
 self.addEventListener('activate', event => {
@@ -11,5 +16,6 @@ self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   if (url.origin == location.origin ) {
     console.log(': service worker sees fetch ' + url.pathname);
+    event.respondWith(caches.match(url.pathname));
   }
 });

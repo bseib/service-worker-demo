993cfff58b7ad85d361689f3d565fcc1a7aa4c47
Broc Seib <broc.seib@gmail.com>
2019-11-16 11:58:37 -0500

    intercept dog.svg and serve cat.svg

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,26 +1,31 @@
 
 self.addEventListener('install', event => {
-  console.log(': service worker version 2 is installing');
+  console.log(': service worker version 3 is installing');
   event.waitUntil(
     caches.open('my-svg-cache').then( cache => {
       cache.add('/index.html');
       cache.add('/dog.svg');
+      cache.add('/cat.svg');
     })
   );
 });
 
 self.addEventListener('activate', event => {
-  console.log(': service worker version 2 has activated');
+  console.log(': service worker version 3 has activated');
 });
 
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   if (url.origin == location.origin ) {
-    console.log(': service worker version 2 sees fetch ' + url.pathname);
+    console.log(': service worker version 3 sees fetch ' + url.pathname);
     switch ( url.pathname ) {
       case '/index.html':
-      case '/dog.svg':
+      case '/cat.svg':
         event.respondWith(caches.match(url.pathname));
+        break;
+      case '/dog.svg':
+          event.respondWith(caches.match('/cat.svg'));
+          break;
       default:
         break;
     }

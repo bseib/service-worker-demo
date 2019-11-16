5bb912b8798bdeaff6816aafc9e1c285d8f497d1
Broc Seib <broc.seib@gmail.com>
2019-11-16 11:51:52 -0500

    cache dog for offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,6 +3,7 @@ self.addEventListener('install', event => {
   console.log(': service worker version 2 is installing');
   event.waitUntil(
     caches.open('my-svg-cache').then( cache => {
+      cache.add('/index.html');
       cache.add('/dog.svg');
     })
   );
@@ -16,6 +17,12 @@ self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   if (url.origin == location.origin ) {
     console.log(': service worker version 2 sees fetch ' + url.pathname);
-    event.respondWith(caches.match(url.pathname));
+    switch ( url.pathname ) {
+      case '/index.html':
+      case '/dog.svg':
+        event.respondWith(caches.match(url.pathname));
+      default:
+        break;
+    }
   }
 });
